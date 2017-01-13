6efdd3adbd9c549b110263e10a82c9648dedc7f8
cac03 <happy96538@gmail.com>
2017-01-13 21:45:27 +0300

    Use separate layout for login activity. So it doesn't contain nav drawer and action bar

--- app/src/main/java/com/caco3/mvk/login/LogInActivity.java
+++ app/src/main/java/com/caco3/mvk/login/LogInActivity.java
@@ -12,7 +12,7 @@ public class LogInActivity extends BaseActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_base);
+    setContentView(R.layout.log_in_activity);
 
     Fragment fragment = findFragment();
     if (fragment == null) {
