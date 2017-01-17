fea268ed451a8690431b407f042e1d17b3833c4c
cac03 <happy96538@gmail.com>
2017-01-17 17:20:19 +0300

    Add onDownloadRequest method

--- app/src/main/java/com/caco3/mvk/audios/AudiosPresenter.java
+++ app/src/main/java/com/caco3/mvk/audios/AudiosPresenter.java
@@ -1,8 +1,9 @@
 package com.caco3.mvk.audios;
 
 import com.caco3.mvk.mvp.BasePresenter;
+import com.caco3.mvk.vk.audio.Audio;
 
 public interface AudiosPresenter extends BasePresenter<AudiosView> {
-  // void onDownloadRequest(Audio);
+  void onDownloadRequest(Audio audio);
   void onRefreshRequest();
 }
