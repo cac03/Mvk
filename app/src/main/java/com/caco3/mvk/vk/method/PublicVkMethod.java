a613e0ace862a6c6edc0bb2f5a915e1f8840500e
cac03 <happy96538@gmail.com>
2017-02-07 00:24:15 +0300

    Remove redundant classes

--- app/src/main/java/com/caco3/mvk/vk/method/PublicVkMethod.java
+++
@@ -1,7 +0,0 @@
-package com.caco3.mvk.vk.method;
-
-/**
- * Vk method that doesn't require {@link com.caco3.mvk.vk.auth.UserToken} to be called
- */
-public abstract class PublicVkMethod<R> extends VkMethod<R>{
-}
