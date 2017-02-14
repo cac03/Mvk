db6fef27ce69108ea636b1c209be1125fde5b4bc
cac03 <happy96538@gmail.com>
2017-02-14 18:12:04 +0300

    Change AudioDownloader interface

--- app/src/main/java/com/caco3/mvk/audiodownload/AudioDownloader.java
+++ app/src/main/java/com/caco3/mvk/audiodownload/AudioDownloader.java
@@ -1,29 +1,8 @@
 package com.caco3.mvk.audiodownload;
 
 
-import com.caco3.mvk.network.FileDownloader;
 import com.caco3.mvk.vk.audio.Audio;
-
-import java.util.List;
-import java.util.Map;
-
-import rx.Observable;
-
 
 public interface AudioDownloader {
   void post(Audio audio);
-  Observable<Progress> getObservable();
-
-  class Progress {
-    /*package*/ Map<Audio, FileDownloader.DownloadProgress> currentlyDownloading;
-    /*package*/ List<Audio> pending;
-
-    public Map<Audio, FileDownloader.DownloadProgress> getCurrentlyDownloading() {
-      return currentlyDownloading;
-    }
-
-    public List<Audio> getPending() {
-      return pending;
-    }
-  }
 }
