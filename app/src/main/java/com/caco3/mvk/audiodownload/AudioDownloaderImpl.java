8122e83d0e919db0956eecc548d0043d5942054f
cac03 <happy96538@gmail.com>
2017-02-15 21:12:43 +0300

    Start handling AudioDownloadEvents in AudioDownloader

--- app/src/main/java/com/caco3/mvk/audiodownload/AudioDownloaderImpl.java
+++ app/src/main/java/com/caco3/mvk/audiodownload/AudioDownloaderImpl.java
@@ -4,11 +4,16 @@ import android.content.Context;
 
 import com.caco3.mvk.vk.audio.Audio;
 
+import rx.android.schedulers.AndroidSchedulers;
+
 public class AudioDownloaderImpl implements AudioDownloader {
   private final Context context;
 
-  public AudioDownloaderImpl(Context context) {
+  public AudioDownloaderImpl(Context context,
+                             AudioDownloadNotificationsSender notificationsSender) {
     this.context = context;
+    // TODO: 2/15/17 When to stop handling?
+    notificationsSender.startHandling(AndroidSchedulers.mainThread());
   }
 
   @Override
--- app/src/main/java/com/caco3/mvk/audios/AudiosModule.java
+++ app/src/main/java/com/caco3/mvk/audios/AudiosModule.java
@@ -2,10 +2,12 @@ package com.caco3.mvk.audios;
 
 import android.content.Context;
 
+import com.caco3.mvk.audiodownload.AudioDownloadNotificationsSender;
 import com.caco3.mvk.audiodownload.storage.AudioDownloadDirectoryProvider;
 import com.caco3.mvk.audiodownload.AudioDownloader;
 import com.caco3.mvk.audiodownload.AudioDownloaderImpl;
 import com.caco3.mvk.loggedin.LoggedInScope;
+import com.caco3.mvk.rxbus.RxBus;
 import com.caco3.mvk.storage.dir.DirectoryProvider;
 
 import dagger.Module;
@@ -22,7 +24,15 @@ public class AudiosModule {
 
   @Provides
   @LoggedInScope
-  public AudioDownloader provideAudioDownloader(Context context) {
-    return new AudioDownloaderImpl(context);
+  public AudioDownloader provideAudioDownloader(Context context,
+                                                AudioDownloadNotificationsSender notificationsSender) {
+    return new AudioDownloaderImpl(context, notificationsSender);
+  }
+
+  @Provides
+  @LoggedInScope
+  public AudioDownloadNotificationsSender provideAudioDownloadNotificationsSender(Context context,
+                                                                                  RxBus rxBus) {
+    return new AudioDownloadNotificationsSender(context, rxBus);
   }
 }
